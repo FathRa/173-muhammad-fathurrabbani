fdbdf3e7fc946b687867dcdf2c8aaa91a421c325
Fathurrabbani <fath.ra001@gmail.com>
2024-05-14 00:59:43 +0700

    Add button component

---
+++ resources/views/components/button/primary.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <button type="submit"
+        class="rounded-md bg-dark-blue p-2 text-white hover:bg-white hover:text-dark-blue hover:ring-2 hover:ring-dark-blue">{{ $slot }}</button>
+</div>
